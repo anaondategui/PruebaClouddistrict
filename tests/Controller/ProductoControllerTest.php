25aa55fcb44530b7200fea7b7771d70d25301155
Ana Belén Ondategui <anaondategui@gmail.com>
2020-08-12 18:34:03 +0200

    Funcionalidad completa, faltan test y limpiar codigo

---
+++ tests/Controller/ProductoControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Controller\ProductoController;
+use PHPUnit\Framework\TestCase;
+
+class ProductoControllerTest extends TestCase
+{
+
+    public function testIndex()
+    {
+
+    }
+}
